c220a2cf65cf320826a08f1c3fbd78185ad05484
Kunozga Mlowoka <kunozgamlowoka@gmail.com>
2021-06-14 20:40:32 +0200

    Implemented soft deletes and search

--- app/Models/Author.php
+++ app/Models/Author.php
@@ -4,14 +4,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Laravel\Scout\Searchable;
 
 class Author extends Model
 {
     use HasFactory;
+    use Searchable;
+    use SoftDeletes;
 
     public function sermons()
     {
         return $this->hasMany("App\Models\Sermon");
+    }
+
+    public function searchableAs()
+    {
+        return "authors_index";
     }
 
     protected $fillable=[
