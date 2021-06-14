8e8eb3757f1377a115ddef83a599f186a66496a5
Kunozga Mlowoka <kunozgamlowoka@gmail.com>
2021-06-14 09:36:07 +0200

    Implemented soft deletes

--- app/Models/Sermon.php
+++ app/Models/Sermon.php
@@ -4,12 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Laravel\Scout\Searchable;
 
 class Sermon extends Model
 {
     use HasFactory;
     use Searchable;
+    use SoftDeletes;
 
     public function author()
     {
