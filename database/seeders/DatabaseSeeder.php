4a51c64084da84d33f5a9be71f0c5cf778513c27
kunozga265 <kunozgamlowoka@gmail.com>
2023-02-26 17:41:16 +0200

    Database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,9 +15,9 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
         $this->call([
-            AuthorTableSeeder::class,
-            SeriesTableSeeder::class,
-            SermonTableSeeder::class,
+//            AuthorTableSeeder::class,
+//            SeriesTableSeeder::class,
+//            SermonTableSeeder::class,
 //            ThemeTableSeeder::class,
 //            CategoryTableSeeder::class,
             UserTableSeeder::class
