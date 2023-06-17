6e60b8245a6793e1824be083a4de198d47caafc8
kunozga265 <kunozgamlowoka@gmail.com>
2023-06-17 17:47:50 +0200

    Added Author Collection

---
+++ app/Http/Resources/AuthorCollection.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\ResourceCollection;
+
+class AuthorCollection extends ResourceCollection
+{
+    /**
+     * Transform the resource collection into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'data'      =>  AuthorResource::collection($this->collection),
+            'meta'      =>  [
+                'current_page'      =>  $this->currentPage(),
+                'total'             =>  $this->total(),
+//                'per_page'          =>  $this->perPage(),
+                'count'             =>  $this->count(),
+                'has_more_pages'    =>  $this->hasMorePages(),
+                'last_page'         =>  $this->lastPage()
+            ]
+        ];
+    }
+}
