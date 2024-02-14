55ebb172b3ce922da4ce36d0a5c9e298d00ac532
kunozga265 <kunozgamlowoka@gmail.com>
2024-02-14 15:49:29 +0200

    Highlight Setup

--- app/Http/Controllers/Web/AppController.php
+++ app/Http/Controllers/Web/AppController.php
@@ -16,39 +16,41 @@ use Illuminate\Support\Facades\Validator;
 
 class AppController extends Controller
 {
-    public $paginate=20;
+    public $paginate = 20;
+    private int $count = 0;
 
     public function dashboard()
     {
         //get sermons
-        $sermons = Sermon::orderBy("published_at","desc")->limit(5)->get();
-        return response()->json(Resources\SermonResource::collection($sermons),200);
+        $sermons = Sermon::orderBy("published_at", "desc")->limit(5)->get();
+        return response()->json(Resources\SermonResource::collection($sermons), 200);
     }
+
     public function dashboard_1_0_0()
     {
         //get sermons
-        $sermons = Sermon::orderBy("published_at","desc")->limit(5)->get();
+        $sermons = Sermon::orderBy("published_at", "desc")->limit(5)->get();
 
         //get prayer points
-        $now=Carbon::now()->getTimestamp();
-        $prayer=Prayer::where('date','<=',$now)->orderBy('date','desc')->first();
+        $now = Carbon::now()->getTimestamp();
+        $prayer = Prayer::where('date', '<=', $now)->orderBy('date', 'desc')->first();
 
         return response()->json([
-            'sermons'   => Resources\SermonResource::collection($sermons),
-            'prayer'    => $prayer
+            'sermons' => Resources\SermonResource::collection($sermons),
+            'prayer' => $prayer
         ]);
     }
 
     public function imageUpload(Request $request)
     {
-        if($request->hasFile('upload')) {
+        if ($request->hasFile('upload')) {
             $originName = $request->file('upload')->getClientOriginalName();
             $fileName = pathinfo($originName, PATHINFO_FILENAME);
             $extension = $request->file('upload')->getClientOriginalExtension();
-            $fileName = $fileName.'_'.time().'.'.$extension;
+            $fileName = $fileName . '_' . time() . '.' . $extension;
             $request->file('upload')->move(public_path('images/uploads'), $fileName);
             $CKEditorFuncNum = $request->input('CKEditorFuncNum');
-            $url = asset('images/uploads/'.$fileName);
+            $url = asset('images/uploads/' . $fileName);
             $msg = 'Image successfully uploaded';
             $response = "<script>window.parent.CKEDITOR.tools.callFunction($CKEditorFuncNum, '$url', '$msg')</script>";
 
@@ -64,42 +66,80 @@ class AppController extends Controller
 
     public function changePassword(Request $request)
     {
-        $user=User::find(Auth::id());
+        $user = User::find(Auth::id());
 
         Validator::make($request->all(), [
-            'currentPassword' => ['required', 'string','min:8'],
-            'newPassword'=> ['required', 'string','min:8']
+            'currentPassword' => ['required', 'string', 'min:8'],
+            'newPassword' => ['required', 'string', 'min:8']
         ])->validate();
 
-        if(Hash::check($request->currentPassword, $user->password)){
+        if (Hash::check($request->currentPassword, $user->password)) {
             $user->forceFill([
                 'password' => Hash::make($request->newPassword),
             ])->save();
-            return Redirect::back()->with('success','Password updated!');
-        }else
-            return Redirect::back()->with('error',"The provided password does not match your current password.");
+            return Redirect::back()->with('success', 'Password updated!');
+        } else
+            return Redirect::back()->with('error', "The provided password does not match your current password.");
 
     }
 
-    public function filterBody($body)
+    public function filterBody($body, $initial_entry = false)
     {
-      $body=json_decode(str_replace('\r\n\t<li>','<li>',json_encode($body)));
-     $body=json_decode(str_replace('\r\n\t\t<li>','<li>',json_encode($body)));
-     $body=json_decode(str_replace('\r\n\t\t\t<li>','<li>',json_encode($body)));
-     $body=json_decode(str_replace('\r\n\t\t<ul>','<ul>',json_encode($body)));
-     $body=json_decode(str_replace('\r\n\t\t<\/ul>','<\/ul>',json_encode($body)));
-     $body=json_decode(str_replace('<li>\r\n\t<p>','<li>',json_encode($body)));
-     $body=json_decode(str_replace('<\/p>\r\n\r\n\t<ul>','\r\n\t<ul>',json_encode($body)));
-     $body=json_decode(str_replace('<\/p>\r\n\r\n\t<ol>','\r\n\t<ol>',json_encode($body)));
-     $body=json_decode(str_replace('<\/ul>\r\n\t<\/li>','<\/ul><\/li>',json_encode($body)));
-     $body=json_decode(str_replace('<\/ul>\r\n\t\t<\/li>','<\/ul><\/li>',json_encode($body)));
-     $body=json_decode(str_replace('<\/ol>\r\n\t<\/li>','<\/ol><\/li>',json_encode($body)));
-     $body=json_decode(str_replace('<\/li>\r\n\t<\/ul>','<\/li><\/ul>',json_encode($body)));
-     $body=json_decode(str_replace('<\/li>\r\n\t<\/ol>','<\/li><\/ol>',json_encode($body)));
-     $body=json_decode(str_replace('<br \/>\r\n\t','<br \/>',json_encode($body)));
-     $body=json_decode(str_replace('<br \/>\t','<br \/>',json_encode($body)));
+
+
+        $body = json_decode(str_replace('\r\n\t<li>', '<li>', json_encode($body)));
+        $body = json_decode(str_replace('\r\n\t\t<li>', '<li>', json_encode($body)));
+        $body = json_decode(str_replace('\r\n\t\t\t<li>', '<li>', json_encode($body)));
+        $body = json_decode(str_replace('\r\n\t\t<ul>', '<ul>', json_encode($body)));
+        $body = json_decode(str_replace('\r\n\t\t<\/ul>', '<\/ul>', json_encode($body)));
+        $body = json_decode(str_replace('<li>\r\n\t<p>', '<li>', json_encode($body)));
+        $body = json_decode(str_replace('<\/p>\r\n\r\n\t<ul>', '\r\n\t<ul>', json_encode($body)));
+        $body = json_decode(str_replace('<\/p>\r\n\r\n\t<ol>', '\r\n\t<ol>', json_encode($body)));
+        $body = json_decode(str_replace('<\/ul>\r\n\t<\/li>', '<\/ul><\/li>', json_encode($body)));
+        $body = json_decode(str_replace('<\/ul>\r\n\t\t<\/li>', '<\/ul><\/li>', json_encode($body)));
+        $body = json_decode(str_replace('<\/ol>\r\n\t<\/li>', '<\/ol><\/li>', json_encode($body)));
+        $body = json_decode(str_replace('<\/li>\r\n\t<\/ul>', '<\/li><\/ul>', json_encode($body)));
+        $body = json_decode(str_replace('<\/li>\r\n\t<\/ol>', '<\/li><\/ol>', json_encode($body)));
+        $body = json_decode(str_replace('<br \/>\r\n\t', '<br \/>', json_encode($body)));
+        $body = json_decode(str_replace('<br \/>\t', '<br \/>', json_encode($body)));
+
+        $body = json_decode(str_replace('<p>', '<p><span>', json_encode($body)));
+        $body = json_decode(str_replace('<\/p>', '<\/span><\/p>', json_encode($body)));
+        $body = json_decode(str_replace('<li>', '<li><span>', json_encode($body)));
+        $body = json_decode(str_replace('<\/li>', '<\/span><\/li>', json_encode($body)));
+
+//        //splits sentences and adds spans
+//        $body = json_decode(preg_replace_callback('/ (\w+)\. (\w+)/', array($this, 'splitSenteces'), json_encode($body)));
+
+        //gives spans ids
+        $body = json_decode(preg_replace_callback('/<(span+)(?![^>]*\/>)[^>]*>/', array($this, 'giveSpanIds'), json_encode($body)));
+
+//        //For highlighting
+//        if($initial_entry){
+//
+//        }else{
+//            dump($body);
+//            //delete all span ids
+//            $body = json_decode(preg_replace_callback('/<span id=\'(0|[1-9][0-9]*)\'>/', array($this, 'temp'), json_encode($body)));
+//
+//            dd($body);
+//
+//            //add new spans
+//
+//            //recalculate ids
+//        }
 
         return $body;
 
+    }
+
+    public function giveSpanIds($matches)
+    {
+        $this->count++;
+        return "<span id='" . $this->count . "'>";
+    }
+    public function splitSenteces($matches)
+    {
+        return " $matches[1]<\/span>. <span>$matches[2]";
     }
 }
