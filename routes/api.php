43f3b9d05c13254b6ff9e4784aa74e2e24fde2b3
Kunozga Mlowoka <kunozgamlowoka@gmail.com>
2025-02-21 14:24:36 +0200

    Retaining downloads

--- routes/api.php
+++ routes/api.php
@@ -115,5 +115,10 @@ Route::group(["prefix"=>"1.2"],function (){
     Route::group(["prefix"=>"transactions", "middleware"=>"auth:sanctum"], function (){
         Route::post('/', [API\V1_2\TransactionController::class, 'store']);
     });
+
+    /* Downloads */
+    Route::group(["prefix"=>"downloads"],function (){
+        Route::get('/',[API\V1_1\DownloadController::class, 'index']);
+    });
 });
 
