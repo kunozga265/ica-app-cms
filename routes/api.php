cb09d95103fb86e90d3309e318d21ddbb7d19255
Kunozga Mlowoka <kunozgamlowoka@gmail.com>
2026-03-03 11:26:52 +0200

    correcting attach members route

--- routes/api.php
+++ routes/api.php
@@ -243,7 +243,7 @@ Route::group(["prefix"=>"1.3.5"],function (){
         Route::post('/', [\App\Http\Controllers\Web\CellController::class, 'store']);
         Route::post('/verify', [\App\Http\Controllers\Web\CellController::class, 'verify']);
         Route::post('/meetings', [\App\Http\Controllers\Web\MeetingController::class, 'store']);
-        Route::post('/attach-members', [API\V1_2\CellController::class, 'attachMembers']);
+        Route::post('/attach-members/{$code}', [API\V1_2\CellController::class, 'attachMembers']);
     });
 
     Route::group(["prefix"=>"meetings", "middleware"=>"auth:sanctum"], function (){
